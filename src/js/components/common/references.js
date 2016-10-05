a12a757636c4a591a58a259ea4931ada71e46d5a
Sebastien <sbstnplcn@gmail.com>
2016-10-05 18:15:49 +0200

    begin carousel references

--- src/js/components/common/references.js
+++ src/js/components/common/references.js
@@ -1,26 +1,40 @@
 ((app) => {
     'use strict'
 
-    var references = [
-        {
-            name : 'Groupe Chèque Déjeuner',
-            image: 'http://www.crt.asso.fr/fileadmin/user_upload/img/UP_ChequeDejuner_2L.jpg'
-        },{
-            name : 'Etanco',
-            image: 'http://static.iquesta.com/logo/iquesta/Etanco_Logo.jpg'
-        },{
-            name : 'Sage',
-            image: 'http://www.sage.co.uk/~/media/markets/uk/images/corporate/sage-green-logo.jpg'
-        },{
-            name : 'Bayern LB',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Bayernlb-logo.svg/2000px-Bayernlb-logo.svg.png'
-        }
-    ]
-
     app.component("references", {
         templateUrl: 'js/components/common/references.html',
-        controller: function($scope) {
-            $scope.references = references
+        controller: function($scope, $timeout, ) {
+            $scope.references = [{
+                name: 'Groupe Chèque Déjeuner',
+                image: 'http://www.crt.asso.fr/fileadmin/user_upload/img/UP_ChequeDejuner_2L.jpg'
+            }, {
+                name: 'Etanco',
+                image: 'http://static.iquesta.com/logo/iquesta/Etanco_Logo.jpg'
+            }, {
+                name: 'Sage',
+                image: 'http://www.sage.co.uk/~/media/markets/uk/images/corporate/sage-green-logo.jpg'
+            }, {
+                name: 'Bayern LB',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Bayernlb-logo.svg/2000px-Bayernlb-logo.svg.png'
+            }, {
+                name: 'Groupe Chèque Déjeuner',
+                image: 'http://www.crt.asso.fr/fileadmin/user_upload/img/UP_ChequeDejuner_2L.jpg'
+            }, {
+                name: 'Etanco',
+                image: 'http://static.iquesta.com/logo/iquesta/Etanco_Logo.jpg'
+            }, {
+                name: 'Sage',
+                image: 'http://www.sage.co.uk/~/media/markets/uk/images/corporate/sage-green-logo.jpg'
+            }, {
+                name: 'Bayern LB',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Bayernlb-logo.svg/2000px-Bayernlb-logo.svg.png'
+            }]
+            $timeout(function() {
+                $scope.references = [{
+                    name: 'Bayern LB',
+                    image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Bayernlb-logo.svg/2000px-Bayernlb-logo.svg.png'
+                }]
+            }, 2000);
         }
     })
 })(angular.module('app.common'))
