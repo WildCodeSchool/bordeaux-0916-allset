7fcdef96561c2fe43049e8909587d1a24803e4f0
uxpaul <hoedevproject@gmail.com>
2016-10-10 16:53:36 +0200

    Add file apropos

---
+++ src/js/components/common/apropos.js
@@ -0,0 +1,6 @@
+((app) => {
+    'use strict'
+    app.component("apropos", {
+            templateUrl: 'js/components/common/apropos.html',
+        }) // app.component
+})(angular.module('app.common'))
