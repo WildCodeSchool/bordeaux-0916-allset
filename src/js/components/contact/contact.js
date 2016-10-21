4ed883fe7994bb9e7d5d7cef44dfe132b636264a
Bully47 <vivien.prog@gmail.com>
2016-10-21 17:26:36 +0200

    j'ai fait une connerie

--- src/js/components/contact/contact.js
+++ src/js/components/contact/contact.js
@@ -2,9 +2,6 @@
     'use strict'
     app.component("contactform", {
         templateUrl: 'js/components/contact/contact.html',
-        controller: function($scope) {
-            $scope.phoneNumbr =  /(0|\+33)[1-9]( *[0-9]{2}){4}/;
-            
-        }
+
     })
 })(angular.module('app.form'))
