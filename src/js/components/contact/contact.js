2bcfd77e4e7500faa3bbbc8001f942af9ec7c0c9
Bully47 <vivien.prog@gmail.com>
2016-10-21 18:55:31 +0200

    uhijo;

--- src/js/components/contact/contact.js
+++ src/js/components/contact/contact.js
@@ -7,9 +7,8 @@
 })(angular.module('app.form'))
 
 
-
-
-//controller: function($scope) {
-//    $scope.phoneNumbr = /(0|\+33)[1-9]( *[0-9]{2}){4}/;
-//
-//}
+// angular.module('validateForm', [])
+//     .controller: function($scope) {
+//         $scope.phoneNumbr = /(0|\+33)[1-9]( *[0-9]{2}){4}/;
+// 
+//     }
