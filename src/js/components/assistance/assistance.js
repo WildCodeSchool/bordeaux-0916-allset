38b19730a195fe6dcedfa8c9e782bb78820b29f3
Sebastien <sbstnplcn@gmail.com>
2016-10-27 10:12:40 +0200

    add textedit in assistance and contactpage

--- src/js/components/assistance/assistance.js
+++ src/js/components/assistance/assistance.js
@@ -1,21 +1,25 @@
 ((app) => {
     'use strict'
     app.component("assistance", {
-        bindings:{
-          editMode:"<",
-          ngModel:"="
+        bindings: {
+            editMode: "<",
+            ngModel: "="
         },
         templateUrl: 'js/components/assistance/assistance.html',
-        controller: function(UserService){
-          angular.extend(this,{
-            connect(){
-              UserService.connect(this.user).then((res) => {
-                //OK
-              }).catch(() => {
-                //ERROR
-              })
-            }
-          })
+        controller: function(UserService) {
+            angular.extend(this, {
+                connect() {
+                    UserService.connect(this.user).then((res) => {
+                        //OK
+                    }).catch(() => {
+                        //ERROR
+                    })
+                },
+                page: {
+                    name: 'home',
+                    content: {}
+                }
+            })
         }
     })
 })(angular.module('app.assistance'))
