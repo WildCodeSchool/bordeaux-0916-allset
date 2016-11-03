e4f7eaf9d7b5cd4e96d6e6e99a599eff5b25953a
Bully47 <vivien.prog@gmail.com>
2016-11-03 09:59:43 +0100

    formulaire terminé sauf liaison back

--- src/js/components/contact/contact.js
+++ src/js/components/contact/contact.js
@@ -6,7 +6,7 @@
             ngModel: "="
         },
         templateUrl: 'js/components/contact/contact.html',
-        controller: [function() {
+        controller: function($state) {
             angular.extend(this, {
                 page: {
                     name: 'home',
@@ -18,8 +18,10 @@
                 saveUser() {
                     var message = this.contactInfo;
                     console.log('SEND !!', message);
+                    toastr.success('Votre message a bien été envoyé', 'Message envoyé');
+                    $state.reload();
                 }
             })
-        }]
+        }
     })
 })(angular.module('app.form'))
